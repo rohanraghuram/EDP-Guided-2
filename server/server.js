bc4c20959a59432e2683fdf35f7460044eebeb91
Alice Smith <alice@webage.com>
2025-03-11 15:21:34 -0400

    planets,characters,films by ID

--- server/server.js
+++ server/server.js
@@ -33,6 +33,90 @@ app.get('/films', async (req, res) => {
     }
 });
 
+app.get('/planets', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(pla_collectionName);
+        const planets = await collection.find({}).toArray();
+        res.json(planets);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/characters', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(char_collectionName);
+        const characters = await collection.find({}).toArray();
+        res.json(characters);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/characters/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(char_collectionName);
+        
+        const characters = await collection.findOne({ id: +id });
+        
+
+        if (!characters) {
+            return res.status(404).json({ error: 'Character not found' });
+        }
+        res.json(characters);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/films/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(film_collectionName);
+        
+        const films = await collection.findOne({ id: +id });
+        
+
+        if (!films) {
+            return res.status(404).json({ error: 'Film not found' });
+        }
+        res.json(films);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+app.get('/planets/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(pla_collectionName);
+        
+        const planets = await collection.findOne({ id: +id });
+        
+
+        if (!planets) {
+            return res.status(404).json({ error: 'PLanet not found' });
+        }
+        res.json(planets);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
