bc1f7477ec1ffbcf2cf097153ff2fd8a74725308
Alice Smith <alice@webage.com>
2025-03-12 11:39:49 -0400

    Fixed homepage which was characterlist, app.jsx, main.jsx, CSS styles. Can now navigate to new character link and go back to homepage. need to implement character component so details of each character can be implemented

---
+++ react/src/components/Home.jsx
@@ -0,0 +1,24 @@
+import CharacterList from "./CharacterList";
+import { useNavigate } from "react-router";
+
+
+const Home = (props) => {
+    const navigate = useNavigate();
+    <h1>Characters</h1>
+    return (
+        <div className="card-container" style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+            <h1>Characters</h1>
+            {
+                props.data.map((character) => (
+                    <div key={character.id} 
+                        onClick={() => navigate(`/characters/${character.id}`)}
+                        style={{ cursor: 'pointer' }}>
+                    <h3>{character.name}</h3>
+                    </div>
+                ))
+            }
+        </div>
+    );
+};
+
+export default Home;
